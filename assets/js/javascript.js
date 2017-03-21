f4b87e319732d988794b35d8bd6aa34b20e1ca6d
Parivash Gumont <Parivash@MacBook-de-Parivash-Gumont.local>
2017-03-20 22:55:36 -0600

    Contenido de README.md desarrollado, comentarios e indentación a html y js

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,7 +1,7 @@
 
-function agregar(){
-    var texto = document.getElementById('agregarFruta').value;
-    var li=document.createElement('li');
-    li.innerHTML=texto==''?'(vacio)':texto;
-    document.getElementById('lista').appendChild(li);
-  }
+function agregar(){ //Función javascript paa agregar elementos a la lista
+    var texto = document.getElementById('agregarFruta').value; // Se crea la variable que almacenará el valor del string que se inserte en el input
+    var li=document.createElement('li'); //Se utiliza el método createElement para crear el nuevo elemento a guardar en la lista
+    li.innerHTML=texto==''?'(vacio)':texto; //Se inserta el valor que tomó la variable texto a la lista
+    document.getElementById('lista').appendChild(li); //Se muestra la lista con el nuevo elemento que se ha agregado
+}
